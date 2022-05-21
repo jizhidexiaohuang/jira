f32dce8d89516405d3eceeb013f7e137eba95e9f
wangfang <18390239389@163.com>
2022-05-21 23:51:10 +0800

    ✨ 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value) => value === 0 ? false : !value
+export const isFalsy = (value: any) => value === 0 ? false : !value
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
     const result = {...object}
     Object.keys(result).forEach(key => {
+        // @ts-ignore
         const value = result[key]
         if(!value){
+            // @ts-ignore
             delete result[key]
         }
     })
     return result
 }
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
     useEffect(() => {
         callback()
     },[])
 }
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
     // 定义一个内部变量
     const [debounceValue, setDebouncedValue] = useState(value)
     // 每次在value变化的时候，设置一个新的定时器
