ed56c99067eddc156bc6d246e2cdf162131ecda9
jzdxh <18390239389@163.com>
2023-09-30 21:21:37 +0800

    feat:添加errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { ProjectListScreen } from "./screens/project-list"
-import { AuthenticatedApp } from './authenticated-app';
-import { UnauthenticatedApp } from './unauthenticated-app';
-import { useAuth } from './context/auth-context';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ProjectListScreen } from "./screens/project-list";
+import { AuthenticatedApp } from "./authenticated-app";
+import { UnauthenticatedApp } from "./unauthenticated-app";
+import { useAuth } from "./context/auth-context";
+import { FullPageErrorFallback } from "./components/lib";
+import { ErrorBoundary } from "./components/error-boundary";
 
 function App() {
-  const {user} = useAuth()
+  const { user } = useAuth();
   return (
     <div className="App">
       {/* <ProjectListScreen /> */}
-      {user ? <AuthenticatedApp />:<UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,29 +1,24 @@
-import React from "react"
+import React from "react";
 
-type FallbackRender = (props:{error:Error|null}) =>React.ReactElement
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
 
 export class ErrorBoundary extends React.Component<
   React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
   { error: Error | null }
->  (
-  state = {error:null}
-  
-  static getDerivedStateFromError(error:Error)=>{
-    return {error}
+> {
+  state = { error: null };
+
+  // 当子组件抛出异常，这里会接收到并且调用
+  static getDerivedStateFromError(error: Error) {
+    return { error };
   }
 
-  render(){
-    const {error} = this.state;
-    const {fallbackRender,children} = this.props;
-
-    if(error){
-      return fallbackRender({error})
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
     }
-    return children
+    return children;
   }
-)
-
-
-
-
-
+}
