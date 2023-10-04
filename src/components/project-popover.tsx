3932ff473f10f54ce9c7ea66d40296680a5330c1
jzdxh <18390239389@163.com>
2023-10-04 18:41:34 +0800

    修改project-popover更新方法

--- src/components/project-popover.tsx
+++ src/components/project-popover.tsx
@@ -7,7 +7,7 @@ import { useProjectModal } from "../screens/project-list/util";
 
 export const ProjectPopover = () => {
   const { open } = useProjectModal();
-  const { data: projects, isLoading } = useProjects();
+  const { data: projects, refetch } = useProjects();
   const pinnedProjects = projects?.filter((project) => project.pin);
 
   const content = (
@@ -27,7 +27,11 @@ export const ProjectPopover = () => {
     </ContentContainer>
   );
   return (
-    <Popover placement={"bottom"} content={content}>
+    <Popover
+      onOpenChange={() => refetch}
+      placement={"bottom"}
+      content={content}
+    >
       <span>项目</span>
     </Popover>
   );
